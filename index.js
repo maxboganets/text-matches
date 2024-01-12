d3e8a5d7073a99f8af005328f7be0aebc9bcedd3
maxboganets <79270958+maxboganets@users.noreply.github.com>
2024-01-12 15:59:15 -0600

    Update index.js
    Add getTextWithHighlightedMatches example into the index.js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {SearchUtility} from './SearchUtility.mjs';
 
-const text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+const text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Dummy me."
 const searchTerm = "dummy";
 
 /*
@@ -19,4 +19,22 @@ console.log(chunks);
 /*
  * RESULT:
  * ["is simply <match>dummy</match> text of t"]
+ */
+
+
+/*
+ * getTextWithHighlightedMatches ARGS:
+ * searchTerm - string
+ * text - string, text to search in
+ * function RETURNS:
+ * text with all the matches wrapped with <match> tag
+ */
+
+const textHighlighted = SearchUtility.getTextWithHighlightedMatches(searchTerm, text);
+
+console.log(textHighlighted);
+
+/*
+ * RESULT:
+ * "Lorem Ipsum is simply <match>dummy</match> text of the printing and typesetting industry. <match>Dummy</match> me."
  */
